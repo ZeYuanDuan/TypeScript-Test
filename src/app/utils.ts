8c2cd73b412820bc0331a9e42ad938284f85fa69
AlsonDuan <alson.duan@subtle-net.com>
2025-05-23 12:12:58 +0800

    test(utils): successfully practice TDD with simple case

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,3 +1,26 @@
 export function toUpperCase(arg: string) {
   return arg.toUpperCase();
 }
+
+export type stringInfo = {
+  lowercase: string;
+  uppercase: string;
+  characters: string[];
+  length: number;
+  extraInfo: Object | undefined;
+};
+
+export function getStringInfo(arg: string): stringInfo {
+  const hasHyphen = arg.includes("-");
+  const extraInfo = {
+    hasHyphen,
+  };
+
+  return {
+    lowercase: arg.toLowerCase(),
+    uppercase: arg.toUpperCase(),
+    characters: Array.from(arg),
+    length: arg.length,
+    extraInfo,
+  };
+}
--- src/test/utils.spec.ts
+++ src/test/utils.spec.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from "../app/utils";
+import { getStringInfo, toUpperCase } from "../app/utils";
 
 describe("utils test suite", () => {
   it("should return uppercase", () => {
@@ -12,5 +12,26 @@ describe("utils test suite", () => {
 
     // Assert
     expect(result).toBe(expectedResult);
+  });
+
+  it("should return info for valid string", () => {
+    // Arrange
+    const sut = getStringInfo;
+    const input = "Test-String";
+    const expectedResult = {
+      lowercase: "test-string",
+      uppercase: "TEST-STRING",
+      characters: ["T", "e", "s", "t", "-", "S", "t", "r", "i", "n", "g"],
+      length: 11,
+      extraInfo: {
+        hasHyphen: true,
+      },
+    };
+
+    // Act
+    const result = sut(input);
+
+    // Assert
+    expect(result).toEqual(expectedResult);
   });
 });
